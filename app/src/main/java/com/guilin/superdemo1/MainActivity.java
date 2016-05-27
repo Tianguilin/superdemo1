0b29685c536d0ddf32097ad1f27517198a97967d
Tian guilin <tianguilin1020@163.com>
2016-05-27 11:57:25 +0800

    双击后退键，退出程序

--- app/src/main/java/com/guilin/superdemo1/MainActivity.java
+++ app/src/main/java/com/guilin/superdemo1/MainActivity.java
@@ -2,6 +2,7 @@ package com.guilin.superdemo1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    private long listTime;
+
+    @Override
+    public void onBackPressed() {
+        long ct=System.currentTimeMillis();
+        if (ct-listTime>2000)
+        {
+            Toast.makeText(this, "再次点击后退退出", Toast.LENGTH_SHORT).show();
+            listTime=ct;
+        }else {
+            super.onBackPressed();
+        }
     }
 }
